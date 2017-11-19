35b3ebe1829b42ddabecd4c3b56681f296be9baf
Aleksey Kabishau <aliaksei.kabishau@gmail.com>
2017-11-19 13:40:02 -0500

    basic set of ARKit

--- World Tracking/ViewController.swift
+++ World Tracking/ViewController.swift
@@ -7,17 +7,20 @@
 //
 
 import UIKit
+import ARKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var sceneView: ARSCNView!
+    let configuration = ARWorldTrackingConfiguration()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+        
+        sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
+        
+        sceneView.session.run(configuration)
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
