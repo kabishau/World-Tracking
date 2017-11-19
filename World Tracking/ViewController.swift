016a1a0898def27209eff23b0c48ad7068bc29a3
Aleksey Kabishau <aliaksei.kabishau@gmail.com>
2017-11-19 14:27:10 -0500

    Adding button Add to UI Adding functionality to button, when it is tapped it places the blue box in the origin of the view

--- World Tracking/ViewController.swift
+++ World Tracking/ViewController.swift
@@ -23,6 +23,16 @@ class ViewController: UIViewController {
 
     }
 
-
+    @IBAction func addButtonTapped(_ sender: UIButton) {
+        // creating node and giving it shape and color and position
+        let node = SCNNode()
+        node.geometry = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0)
+        node.geometry?.firstMaterial?.diffuse.contents = UIColor.blue
+        node.position = SCNVector3(0, 0, 0)
+        // scene - what is visible on screen
+        // rootNode - by default set to origin of the view, has no shape, color, etc
+        sceneView.scene.rootNode.addChildNode(node)
+    }
+    
 }
 
