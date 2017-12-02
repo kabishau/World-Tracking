4b49741cb065b2633121f345bdf585331b1ab9a8
user131656 <user131656@ny218.local>
2017-12-01 19:09:26 -0500

    Setting up the cell in the collection view

--- World Tracking/IkeaViewController.swift
+++ World Tracking/IkeaViewController.swift
@@ -9,7 +9,9 @@
 import UIKit
 import ARKit
 
-class IkeaViewController: UIViewController {
+class IkeaViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    let itemsArray: [String] = ["cup", "vase", "boxing", "table"]
 
     @IBOutlet weak var sceneView: ARSCNView!
     @IBOutlet weak var itemsCollectionView: UICollectionView!
@@ -22,6 +24,32 @@ class IkeaViewController: UIViewController {
         sceneView.session.run(configuration)
         sceneView.debugOptions = [ARSCNDebugOptions.showWorldOrigin, ARSCNDebugOptions.showFeaturePoints]
         
-
+        // setting the delegate object
+        itemsCollectionView.delegate = self
+        
     }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return itemsArray.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Item", for: indexPath) as! ItemCell
+        cell.itemLabel.text = itemsArray[indexPath.row]
+        return cell
+    }
+    
+    // UICollectionViewDelegate function
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let cell = collectionView.cellForItem(at: indexPath)
+        cell?.backgroundColor = #colorLiteral(red: 1, green: 0.5781051517, blue: 0, alpha: 1)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
+        let cell = collectionView.cellForItem(at: indexPath)
+        cell?.backgroundColor = #colorLiteral(red: 1, green: 0.8323456645, blue: 0.4732058644, alpha: 1)
+        
+    }
+    
+    
 }
---
+++ World Tracking/ItemCell.swift
@@ -0,0 +1,16 @@
+//
+//  ItemCell.swift
+//  World Tracking
+//
+//  Created by user131656 on 12/1/17.
+//  Copyright © 2017 Aleksey Kabishau. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UICollectionViewCell {
+    
+    @IBOutlet weak var itemLabel: UILabel!
+    
+    
+}
