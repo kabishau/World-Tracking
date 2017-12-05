1c825fee850bcdb84785ac2ac5d1ada49bb072da
Aleksey Kabishau <aliaksei.kabishau@gmail.com>
2017-12-04 21:14:32 -0500

    basic set up for plane detection

---
+++ World Tracking/PlaneViewController.swift
@@ -0,0 +1,19 @@
+import UIKit
+import ARKit
+
+class PlaneViewController: UIViewController {
+
+    @IBOutlet weak var sceneView: ARSCNView!
+    
+    let configuration = ARWorldTrackingConfiguration()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
+        configuration.planeDetection = .horizontal
+        sceneView.session.run(configuration)
+
+
+    }
+}
