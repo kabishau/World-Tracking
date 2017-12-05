ae88a7d975fcf11494f48fc1661869d0745962e0
Aleksey Kabishau <aliaksei.kabishau@gmail.com>
2017-12-04 23:14:05 -0500

    creating horizontal lava sample on the detected horizontal surface using ARPlaneAnchor

--- World Tracking/PlaneViewController.swift
+++ World Tracking/PlaneViewController.swift
@@ -14,30 +14,28 @@ class PlaneViewController: UIViewController, ARSCNViewDelegate {
         configuration.planeDetection = .horizontal
         sceneView.session.run(configuration)
         sceneView.delegate = self
-        
-        let lavaNode = createLava()
-        sceneView.scene.rootNode.addChildNode(lavaNode)
-        
-        
     }
     
-    func createLava() -> SCNNode {
-        let lavaNode = SCNNode(geometry: SCNPlane(width: 1, height: 1))
+    func createLava(planeAnchor: ARPlaneAnchor) -> SCNNode {
+        let lavaNode = SCNNode(geometry: SCNPlane(width: CGFloat(planeAnchor.extent.x), height: CGFloat(planeAnchor.extent.z)))
         lavaNode.geometry?.firstMaterial?.diffuse.contents = #imageLiteral(resourceName: "Lava")
         // covering the surface of the Plane for both sides
         lavaNode.geometry?.firstMaterial?.isDoubleSided = true
-        lavaNode.position = SCNVector3(0, 0, -1)
+        lavaNode.position = SCNVector3(planeAnchor.center.x, planeAnchor.center.y, planeAnchor.center.z)
         lavaNode.eulerAngles = SCNVector3(Float(90.degreesToRadians), 0, 0)
         return lavaNode
     }
     
     func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
         guard let planeAnchor = anchor as? ARPlaneAnchor else { return }
+        let lavaNode = createLava(planeAnchor: planeAnchor)
+        node.addChildNode(lavaNode)
         print("new flat surface is detected and new ARPlaneAnchor has been created")
     }
     
     func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
         guard let planeAnchor = anchor as? ARPlaneAnchor else { return }
+        
         print("updating floor's anchor")
     }
     
