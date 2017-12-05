b88eb95a4d4322618d01790d9bff165edb717099
Aleksey Kabishau <aliaksei.kabishau@gmail.com>
2017-12-04 21:37:40 -0500

    using ARSCNDelegate methods for detecting horizontal surface and creating Plane Anchor (one for one surface)

--- World Tracking/PlaneViewController.swift
+++ World Tracking/PlaneViewController.swift
@@ -1,7 +1,7 @@
 import UIKit
 import ARKit
 
-class PlaneViewController: UIViewController {
+class PlaneViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet weak var sceneView: ARSCNView!
     
@@ -13,7 +13,21 @@ class PlaneViewController: UIViewController {
         sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
         configuration.planeDetection = .horizontal
         sceneView.session.run(configuration)
-
-
+        sceneView.delegate = self
     }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        guard let planeAnchor = anchor as? ARPlaneAnchor else { return }
+        print("new flat surface is detected and new ARPlaneAnchor has been created")
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+        guard let planeAnchor = anchor as? ARPlaneAnchor else { return }
+        print("updating floor's anchor")
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didRemove node: SCNNode, for anchor: ARAnchor) {
+        print("second anchor has been removed")
+    }
+    
 }
